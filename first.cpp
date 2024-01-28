8994b85172a40d807d2485853f98a533d86a4ac8
chandandass <cdas99633@gmail.com>
2024-01-29 01:46:58 +0530

    test commit

---
+++ first.cpp
@@ -0,0 +1,26 @@
+#include "header.h"
+#include <iostream>
+
+using namespace std;
+
+int Func(int, int);
+
+int main() {
+  int a = Func(10, 10);
+  cout << "value of a : " << a;
+  return 0;
+}
+
+int Func(int a, int b) {
+
+  return a+b;
+}
+
+
+class Abc {
+ int main;
+private:
+protected:
+public:
+ 
+};
